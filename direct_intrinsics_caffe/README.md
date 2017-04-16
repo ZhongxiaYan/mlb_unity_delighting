e99e382749bb086c88c37634ca3daaa89f442ed2
Zhongxia Yan <zxyan@berkeley.edu>
2017-04-16 12:13:08 -0700

    added direct_intrinsics for caffe and added a util file

---
+++ direct_intrinsics_caffe/README.md
@@ -0,0 +1,30 @@
+# Direct Intrinsics
+
+This is a project web site of our work on ICCV 2015 described in:
+
+> [Direct Intrinsics: Learning Albedo-Shading Decomposition by Convolutional Regression](http://www.cv-foundation.org/openaccess/content_iccv_2015/papers/Narihira_Direct_Intrinsics_Learning_ICCV_2015_paper.pdf)
+> [arxiv](http://arxiv.org/abs/1512.02311)
+> Takuya Narihira, Michael Maire, and Stella X. Yu
+> International Conference on Computer Vision (ICCV), 2015
+
+# Code release
+## Run trained model
+* See [model_release](model_release)
+
+## Training a new model
+* [Prepare Dataset](data)
+* [How to train](training)
+
+## Citation
+
+If you make use of this software, please cite the following in any publications:
+
+    @inproceedings{NMY:ICCV:2015,
+        title     = {Direct Intrinsics: Learning Albedo-Shading Decomposition by Convolutional Regression},
+        author    = {Takuya Narihira, Michael Maire and Stella X. Yu},
+        booktitle = {International Conference on Computer Vision (ICCV)},
+        year      = {2015}
+    }
+
+
+## License
---
+++ util.py
@@ -0,0 +1,42 @@
+import os
+
+def scan_lighted_delighted(date_dir):
+    '''
+    date_dir: i.e. Data_2017_03_31/ WITH THE FORWARD SLASH
+    returns:
+        delighted_dirs: maps mesh (Mesh_000000) to directory of delighted data
+            ('data/Data_2017_03_31/Data/Rock/Mesh_000000/_Shared/')
+        lighted_dirs: a list of (type, mesh, version, lighted_directory_path) tuples,
+            i.e. (Rock, Mesh_000000, V_000014, 'data/Data_2017_03_31/Data/Rock/Mesh_000000/V_000014/')
+    '''
+    data_dir = date_dir + 'Data/'
+    delighted_dirs = {}
+    lighted_dirs = []
+    types = os.listdir(data_dir)
+    for type in types:
+        type_dir = data_dir + type + '/'
+        meshes = [mesh for mesh in os.listdir(type_dir) if mesh.startswith('Mesh_')]
+        for mesh in meshes:
+            mesh_dir = type_dir + mesh + '/'
+            delighted_dir = mesh_dir + '_Shared/'
+            if mesh not in delighted_dirs:
+                delighted_dirs[mesh] = delighted_dir
+            delighted_dir = delighted_dirs[mesh]
+            versions = [v for v in os.listdir(mesh_dir) if v.startswith('V_')]
+            for version in versions:
+                version_dir = mesh_dir + version + '/'
+                lighted_dirs.append((type, mesh, version, version_dir))
+    return delighted_dirs, lighted_dirs
+
+def zero_rgb(im):
+    '''
+    takes in a r, g, b, a numpy array and zero out rgb if a = 0
+    '''
+    alpha = im[:, :, 3:] > 0
+    return im * alpha
+
+def downsample_image(image, factor):
+    '''
+    takes in a pillow image object, low pass, then downsample
+    '''
+    return image.resize((image.width // factor, image.height // factor), Image.ANTIALIAS)
