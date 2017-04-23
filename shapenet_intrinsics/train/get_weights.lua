78d653dc6ddd9b07af1db6a03049c0f725301cf4
Ubuntu <ubuntu@ip-172-31-42-225.us-west-2.compute.internal>
2017-04-23 04:56:38 +0000

    added model_weights

---
+++ shapenet_intrinsics/train/get_weights.lua
@@ -0,0 +1,73 @@
+require 'torch'
+require 'paths'
+require 'image'
+require 'nn'
+require 'nngraph'
+require 'cunn'
+require 'cudnn'
+
+local cmd = torch.CmdLine()
+
+cmd:option('-input', '', 'input image')
+cmd:option('-mask', '', 'input mask')
+cmd:option('-model', '', 'model file')
+cmd:option('-outdir', '.', 'output directory')
+cmd:option('-gpu', 1, 'use GPU')
+
+local options = cmd:parse(arg)
+
+-- load model
+local model = torch.load('model.t7')
+
+--print(model.modules[1].weights)
+--[[print('-----')
+print(#model:float():parameters())
+print('-----')
+print(model:parameters()[1])
+--]]
+params = model:parameters()
+file = io.open("model_weights.txt", "w")
+local arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 19, 22, 25, 28, 31, 34, 35}
+for _, i in ipairs(arr) do
+  file:write(tostring(params[4 * (i - 1) + 1]))
+  file:write(tostring(params[4 * (i - 1) + 2]))
+  file:write(tostring(params[4 * (i - 1) + 3]))
+  file:write(tostring(params[4 * (i - 1) + 4]))
+  print(i * 4)
+end
+
+file:close()
+--[[
+-- load input image and mask
+input[{1, {}, {}, {}}] = image.scale(image.load(options.input, 3), 256, 256)
+--mask[{1, {}, {}, {}}]  = image.scale(image.load(options.mask,  3), 256, 256)
+
+if options.gpu == 0 then
+  model:float()
+else  
+  model:cuda()
+  input = input:cuda()
+  --mask = mask:cuda()
+end
+
+local pred = model:forward(input)
+
+
+image.save(paths.concat(options.outdir, 'albedo.png'), pred[1]:squeeze())
+image.save(paths.concat(options.outdir, 'shading.png'), pred[2]:squeeze())
+image.save(paths.concat(options.outdir, 'specular.png'), pred[3]:squeeze())
+
+--EOF
+
+
+
+-- save output
+image.save(paths.concat(options.outdir, 'albedo.png'), pred[1]:cmul(mask):squeeze())
+image.save(paths.concat(options.outdir, 'shading.png'), pred[2]:cmul(mask):squeeze())
+image.save(paths.concat(options.outdir, 'specular.png'), pred[3]:cmul(mask):squeeze())
+-- save a copy of input
+image.save(paths.concat(options.outdir, 'input.png'), input:squeeze())
+image.save(paths.concat(options.outdir, 'mask.png'), mask:squeeze())
+
+--EOF
+--]]
