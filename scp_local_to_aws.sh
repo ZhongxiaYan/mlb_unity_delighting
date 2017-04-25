59b5984a5c815dc3840eec6a0dfe8d413e1c20b6
Zhongxia Yan <zxyan@berkeley.edu>
2017-04-25 12:23:20 -0700

    updated shapenet to full arch

--- scp_local_to_aws.sh
+++ scp_local_to_aws.sh
@@ -2,4 +2,4 @@
 # "bash scp_local_to_aws.sh local_source_path remote_dest_path"
 # local_source_path: something like a/b/c/file.txt
 # remote_dest_path: something like ~/output_dir/file.txt
-scp -i berkeley_unity_delighting.pem $1 ubuntu@ec2-35-163-238-38.us-west-2.compute.amazonaws.com:$2
+scp -r -i berkeley_unity_delighting.pem $1 ubuntu@ec2-35-163-238-38.us-west-2.compute.amazonaws.com:$2
